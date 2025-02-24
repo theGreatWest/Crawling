f0f4025cb737ae63f9d09deeecd56646d1f613cf
youngjuKim <kjuzoojuk@naver.com>
2025-02-24 19:13:46 +0900

    feat: KakaoAddressAPI 클래스를 통해 위도와 경도를 얻어오는 부분 추가

--- src/main/java/kr/book/search/controller/AppController.java
+++ src/main/java/kr/book/search/controller/AppController.java
@@ -2,6 +2,7 @@ package kr.book.search.controller;
 
 import kr.book.search.model.Book;
 import kr.book.search.service.AppService;
+import kr.book.search.util.api.KakaoAddressAPI;
 import kr.book.search.util.api.KakaoBookSearchAPI;
 import kr.book.search.util.pdf.PdfGenerator;
 import kr.book.search.view.InputView;
@@ -19,30 +20,46 @@ public class AppController {
         this.appService = appService;
     }
 
-    public void run(){
-        String title = inputBookTitle();
+    public void run() {
+//        String title = inputBookTitle();
+//
+//        List<Book> books = getBooks(title);
+//
+//        createPDF(books);
 
-        List<Book> books = getBooks(title);
+        String address = inputAddress();
 
-        createPDF(books);
+        findCoordinate(address);
     }
 
-    private String inputBookTitle(){
+    private String inputBookTitle() {
         String title = null;
-        while(title==null){
+        while (title == null) {
             outputView.printMessage("도서 제목을 입력하세요");
             title = inputView.userRequest();
-            if(title==null){
+            if (title == null) {
                 outputView.printMessage("다시 입력해 주세요\n");
             }
         }
         return title;
     }
 
-    private List<Book> getBooks(String title){
+    private String inputAddress() {
+        String address = null;
+        while (address == null) {
+            outputView.printMessage("주소를 입력하세요");
+            address = inputView.userRequest();
+            if (address == null) {
+                outputView.printMessage("다시 입력해 주세요\n");
+            }
+        }
+        return address;
+    }
+
+    private List<Book> getBooks(String title) {
         List<Book> books = KakaoBookSearchAPI.searchBooks(title);
 
-        if(books == null) {
+        if (books == null) {
             outputView.printMessage("검색 결과가 없습니다.");
             return null;
         }
@@ -50,7 +67,21 @@ public class AppController {
         return books;
     }
 
-    private void createPDF(List<Book> books){
+    private void createPDF(List<Book> books) {
         PdfGenerator.generateBookListPdf(books, "result");
     }
+
+    private void findCoordinate(String address){
+        double[] coordinate = KakaoAddressAPI.getAddressCoordinate(address);
+
+        if(coordinate != null){
+            outputView.printMessage("주소: "+address);
+            outputView.printMessage("위도: "+coordinate[0]);
+            outputView.printMessage("경도: "+coordinate[1]);
+        }else{
+            outputView.printMessage("주소를 찾을 수 없습니다.");
+        }
+    }
+
+
 }
