5dc78b9cfc2fb253ea8fd91aefec0e9e26b604b4
youngjuKim <kjuzoojuk@naver.com>
2025-02-24 15:34:18 +0900

    feat: input view 에 사용자 입력 메서드 추가

--- src/main/java/kr/book/search/view/InputView.java
+++ src/main/java/kr/book/search/view/InputView.java
@@ -5,5 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputView {
+    public String userRequest(){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+            return br.readLine();
+        }catch (IOException e){
+            System.out.println("검색에 실패했습니다.");
+
+            return null;
+        }
+    }
 }
